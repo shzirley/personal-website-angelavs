75040923f32a455f75529c70e2584a7ead226368
enjigg <angelasugiyono95@gmail.com>
2026-09-14 15:55:26 +0700

    fix: generate HTTPS asset URLs behind Azure proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::share('profile', config('portfolio'));
     }
 }
